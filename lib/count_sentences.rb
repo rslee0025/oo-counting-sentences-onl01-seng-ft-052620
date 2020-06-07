c088c59b64a6dabd188a338694cb4a809567aed8
Rachel S. Lee <rslee0025@gmail.com>
2020-06-07 23:42:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
-  end
-end
+    sentence = self.split(/[.?!]/)
+    sentence.delete("")
+    sentence.count
+  end	  
+  
+end   
